4e17966829b3a73060bb34290149441d68a4931d
Aashish <aashish0698@gmail.com>
2019-07-14 11:10:50 +0530

    finally donegit add -A

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Blog
+def blog(request):
+	blogs=Blog.objects
+	return render(request,'blog/blog.html',{"blogs":blogs})
